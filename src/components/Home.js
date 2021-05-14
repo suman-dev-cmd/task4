6134d4f2e3a06d9b347d485e539380fe2e0bb3c4
suman-dev-cmd <sumanj@capitalnumbers.com>
2021-05-14 17:14:26 +0530

    loader add

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Skeleton from '@material-ui/lab/Skeleton';
+import Backdrop from '@material-ui/core/Backdrop';
 import {get} from '../utils/service'
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -17,68 +20,84 @@ const useStyles = makeStyles((theme) => ({
     control: {
       padding: theme.spacing(10),
     },
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: '#fff',
+        // backgroundColor:'black'
+      },
   }));
 const Home = () => {
     const [spacing, setSpacing] = useState(8);
     const classes = useStyles();
     const [image,setImage] = useState([]);
-    const [imageslice,setImageslice] = useState([]);
-    const [offset, setOffset] = useState(0);
-    const [perpage, setPerpage] = useState(8);
-    const [imagelength,setImagelenght] = useState(0)
+    // const [imageslice,setImageslice] = useState([]);
+    // const [offset, setOffset] = useState(0);
+    const [page, setPage] = useState(1);
+    const [limit, setLimit] = useState(8);
+    // const [loading, setLoading] = useState(false);
+    // const [imagelength,setImagelenght] = useState(0)
+    const [open, setOpen] = useState(true);
 
 
     useEffect(() => {   
-         get('/v2/list')
+        console.log(page)
+        console.log(limit)
+         get(`/v2/list?page=${page}&limit=${limit}`)
          .then(res=>{
              console.log(res.data)
              setImage(res.data)
-             setImagelenght(res.data.length)
-             const slice = res.data.slice(offset,offset+perpage)
-             setImageslice(slice)
+             setTimeout(()=>{
+             setOpen(false)
+             },2000)
+
+            //  setImagelenght(res.data.length)
+            //  const slice = res.data.slice(offset,offset+perpage)
+            //  setImageslice(slice)
          })
          .catch(err=>{
              console.log(err)
          })
-    },[])
+    },[page,limit])
   
     const loadMore = () => {
-      const nextItem = perpage + 8 
-      console.log(perpage)
-      console.log(imagelength)
-      console.log(nextItem)
-      
-        setPerpage(nextItem)
-        const slicemore = image.slice(offset,offset+nextItem)
-        setImageslice(slicemore)
-      
+    
+      const nextItem = page + 1 
+      const nextlimit = limit + 8
+        setPage(nextItem)
+        setLimit(nextlimit)
+        setOpen(true)
     };
   
     return (
       <Grid container className={classes.root} spacing={10}>
+        <Backdrop className={classes.backdrop} open={open} >
+            <CircularProgress color="inherit" />
+        </Backdrop>
         <Grid item xs={12}>
           <Grid container justify="center" spacing={spacing}>
-            {imageslice.map((value,i) => (
-              <Grid key={i} item>
-                {/* <Paper className={classes.paper} /> */}
-                <img src={value.download_url} className={classes.paper}/>
-              </Grid>
-            ))}
+            {
+                image.map.length !== 0?
+                image.map((value,i) => (
+                    <Grid key={i} item>
+                      {/* <Paper className={classes.paper} /> */}
+                      <img src={value.download_url} className={classes.paper}/>
+                    </Grid>
+                  ))
+                :
+                null
+
+            }
+            
           </Grid>
         </Grid>
         <Grid item xs={12} className='loadmore'>
-            {
-                perpage <= imagelength ?
                 <center>
                     <Button variant="contained" color="primary" onClick={loadMore}>
                         Load More
                     </Button>
                 </center>
-                :
-                null
-            }
-           
             
+                
        </Grid>
       </Grid>
     );
